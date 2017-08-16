0b75656621033808114e29185503e3248779a315
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-08-16 19:38:12 +0200

    Next stop

---
+++ src/main/java/AutomatZBatinami.java
@@ -0,0 +1,7 @@
+/**
+ * Created by RENT on 2017-08-16.
+ */
+public class AutomatZBatinami {
+    public AutomatZBatinami(Kasa kasa, Produkty produkty) {
+    }
+}
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -9,6 +9,6 @@ Kasa kasa;
 @Mock
 Produkty produkty;
 
-
+AutomatZBatinami testObject=new AutomatZBatinami(kasa,produkty);
 
 
